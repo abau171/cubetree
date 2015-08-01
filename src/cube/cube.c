cf540db7f56365d3e9d0709d4b421fe3e9ce17c2
Andrew Bauer <abau171@gmail.com>
2015-08-01 11:43:38 -0400

    added clockwise quarter turns

--- src/cube/cube.c
+++ src/cube/cube.c
@@ -12,6 +12,10 @@ struct Cube rightCube = {{{DFR, COUNTER_ROT}, {UFL, 0}, {UBL, 0}, {UFR, CLOCKWIS
 
 struct Cube frontCube = {{{UFL, CLOCKWISE_ROT}, {DFL, COUNTER_ROT}, {UBL, 0}, {UBR, 0}, {UFR, COUNTER_ROT}, {DFR, CLOCKWISE_ROT}, {DBL, 0}, {DBR, 0}}, {{FL, FLIP}, {UL, 0}, {UB, 0}, {UR, 0}, {UF, FLIP}, {DF, FLIP}, {BL, 0}, {BR, 0}, {FR, FLIP}, {DL, 0}, {DB, 0}, {DR, 0}}};
 
+struct Cube FRCube = {{{UFR, 1}, {DFL, 2}, {UBL, 0}, {UFL, 2}, {DBR, 1}, {DFR, 1}, {DBL, 0}, {UBR, 2}}, {{FL, 1}, {UL, 0}, {UB, 0}, {UF, 1}, {DR, 0}, {DF, 1}, {BL, 0}, {UR, 0}, {FR, 1}, {DL, 0}, {DB, 0}, {BR, 0}}};
+
+struct Cube boringCube = {{{UFR, 0}, {UFR, 0}, {UFR, 0}, {UFR, 0}, {UFR, 0}, {UFR, 0}, {UFR, 0}, {UFR, 0}}, {{UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}, {UF, 0}}};
+
 Cube newCube() {
 	Cube cube = malloc(sizeof(struct Cube));
 	*cube = solvedCube;
--- src/cube/cube_turn.c
+++ src/cube/cube_turn.c
@@ -1,19 +1,78 @@
 #include "cube.h"
 #include "cube_internal.h"
 
-void turnCubeFace(Cube cube, CubeFaceId face, TurnType type) {
-	switch (face) {
-	case U:
+static CubeCornerId cornerQuarterRevolutions[6][4] = {
+	{0, 1, 2, 3},
+	{5, 6, 2, 1},
+	{4, 5, 1, 0},
+	{7, 4, 0, 3},
+	{6, 7, 3, 2},
+	{7, 6, 5, 4}};
+
+static void quarterRevolveCorners(Cube cube, CubeFaceId faceId) {
+	struct Corner tmp = cube->corners[cornerQuarterRevolutions[faceId][3]];
+	cube->corners[cornerQuarterRevolutions[faceId][3]] = cube->corners[cornerQuarterRevolutions[faceId][2]];
+	cube->corners[cornerQuarterRevolutions[faceId][2]] = cube->corners[cornerQuarterRevolutions[faceId][1]];
+	cube->corners[cornerQuarterRevolutions[faceId][1]] = cube->corners[cornerQuarterRevolutions[faceId][0]];
+	cube->corners[cornerQuarterRevolutions[faceId][0]] = tmp;
+}
+
+static CubeCornerRotation cornerQuarterRotations[6][4] = {
+	{NO_ROT, NO_ROT, NO_ROT, NO_ROT},
+	{CLOCKWISE_ROT, COUNTER_ROT, CLOCKWISE_ROT, COUNTER_ROT},
+	{CLOCKWISE_ROT, COUNTER_ROT, CLOCKWISE_ROT, COUNTER_ROT},
+	{CLOCKWISE_ROT, COUNTER_ROT, CLOCKWISE_ROT, COUNTER_ROT},
+	{CLOCKWISE_ROT, COUNTER_ROT, CLOCKWISE_ROT, COUNTER_ROT},
+	{NO_ROT, NO_ROT, NO_ROT, NO_ROT}};
+
+static void quarterRotateCorners(Cube cube, CubeFaceId faceId) {
+	for (int i = 0; i < 4; i++) {
+		cube->corners[cornerQuarterRevolutions[faceId][i]].rotation = wrapCornerRotation(cube->corners[cornerQuarterRevolutions[faceId][i]].rotation + cornerQuarterRotations[faceId][i]);
+	}
+}
+
+static CubeEdgeId edgeQuarterRevolutions[6][4] = {
+	{0, 1, 2, 3},
+	{9, 6, 1, 5},
+	{8, 5, 0, 4},
+	{11, 4, 3, 7},
+	{10, 7, 2, 6},
+	{10, 9, 8, 11}};
+
+static void quarterRevolveEdges(Cube cube, CubeFaceId faceId) {
+	struct Edge tmp = cube->edges[edgeQuarterRevolutions[faceId][3]];
+	cube->edges[edgeQuarterRevolutions[faceId][3]] = cube->edges[edgeQuarterRevolutions[faceId][2]];
+	cube->edges[edgeQuarterRevolutions[faceId][2]] = cube->edges[edgeQuarterRevolutions[faceId][1]];
+	cube->edges[edgeQuarterRevolutions[faceId][1]] = cube->edges[edgeQuarterRevolutions[faceId][0]];
+	cube->edges[edgeQuarterRevolutions[faceId][0]] = tmp;
+}
+
+static CubeEdgeFlip edgeQuarterFlips[6][4] = {
+	{NO_FLIP, NO_FLIP, NO_FLIP, NO_FLIP},
+	{NO_FLIP, NO_FLIP, NO_FLIP, NO_FLIP},
+	{FLIP, FLIP, FLIP, FLIP},
+	{NO_FLIP, NO_FLIP, NO_FLIP, NO_FLIP},
+	{FLIP, FLIP, FLIP, FLIP},
+	{NO_FLIP, NO_FLIP, NO_FLIP, NO_FLIP}};
+
+static void quarterFlipEdges(Cube cube, CubeFaceId faceId) {
+	for (int i = 0; i < 4; i++) {
+		cube->edges[edgeQuarterRevolutions[faceId][i]].flip = wrapEdgeFlip(cube->edges[edgeQuarterRevolutions[faceId][i]].flip + edgeQuarterFlips[faceId][i]);
+	}
+}
+
+void turnCubeFace(Cube cube, CubeFaceId faceId, TurnType type) {
+	switch (type) {
+	case DOUBLE_TURN:
 		break;
-	case L:
+	case CLOCKWISE_TURN:
+	case COUNTER_TURN:
+		quarterRotateCorners(cube, faceId);
+		quarterRevolveCorners(cube, faceId);
+		quarterFlipEdges(cube, faceId);
+		quarterRevolveEdges(cube, faceId);
 		break;
-	case F:
-		break;
-	case R:
-		break;
-	case B:
-		break;
-	case D:
+	default:
 		break;
 	}
 }
