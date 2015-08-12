9c2a66880f8877d9cd2eb16b2f793185582e76a7
Andrew Bauer <abau171@gmail.com>
2015-08-11 20:04:49 -0400

    documented main cube header

--- src/puzzles/cube3x3_puzzle/cube3x3/cube.h
+++ src/puzzles/cube3x3_puzzle/cube3x3/cube.h
@@ -9,12 +9,23 @@
 #define NUM_CUBE_CORNERS 8
 #define NUM_CUBE_EDGES 12
 
+/*
+ * Ids for each face of a cube.
+ */
 typedef int8_t CubeFaceId;
 enum CubeFaceId { U_FACE, L_FACE, F_FACE, R_FACE, B_FACE, D_FACE, NO_FACE = -1 };
 
+/*
+ * Ids for each type of turn for a cube face.
+ *
+ * Integer values also correspond to the number of clocwise turns needed to achieve the desired state.
+ */
 typedef int8_t TurnType;
 enum TurnType { NO_TURN, CLOCKWISE_TURN, DOUBLE_TURN, COUNTER_TURN };
 
+/*
+ * Cube is defined as a pointer to a Cube structure.
+ */
 typedef struct Cube* Cube;
 
 Cube newCube();
